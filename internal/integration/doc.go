56bc6b3540715c811aa94e11752972ac812030da
James Harris <contact@jamesharris.id.au>
2025-11-29 08:46:55 +1000

    Remove integration code.

--- internal/integration/doc.go
+++
@@ -1,4 +0,0 @@
-// Package integration is the subsystem responsible for maintaining the state
-// and behavior of [dogma.IntegrationMessageHandler] implementations within an
-// application.
-package integration
--- internal/integration/internal/wal/doc.go
+++
@@ -1,4 +0,0 @@
-// Package wal encapsulates the integration subsystem's use of
-// a [journal.Store] as a write-ahead log (WAL) for recording the
-// operations it performs.
-package wal
