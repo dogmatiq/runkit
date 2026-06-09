ee52707796c263c7315025735291a6ccd34fcf18
James Harris <contact@jamesharris.id.au>
2026-06-09 14:22:41 +1000

    Favour returning context cancellation error from xsql.Transact().

--- internal/x/xsql/tx.go
+++ internal/x/xsql/tx.go
@@ -14,7 +14,21 @@ func Transact(
 	ctx context.Context,
 	db *sql.DB,
 	fn func(context.Context, *sql.Tx) error,
-) error {
+) (err error) {
+	defer func() {
+		// If the context was canceled, return that error instead of any other
+		// error from fn or database operations.
+		//
+		// This ensures that if the transaction is rolled back due to context
+		// cancellation, the caller receives the context cancellation error
+		// instead of an error from fn that may be a consequence of the
+		// rollback, which in practice can include [sql.ErrTxDone],
+		// [driver.ErrBadConn] as well as internal PostgreSQL errors.
+		if ctx.Err() != nil {
+			err = ctx.Err()
+		}
+	}()
+
 	tx, err := db.BeginTx(ctx, nil)
 	if err != nil {
 		return fmt.Errorf("unable to begin transaction: %w", err)
