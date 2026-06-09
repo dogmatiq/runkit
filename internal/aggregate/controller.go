15df6c44a17f86076a8d7cbbbae64eca401f712f
James Harris <contact@jamesharris.id.au>
2026-06-09 18:15:06 +1000

    Execute aggregate commands transactionally, skip locked rows.

--- internal/aggregate/controller.go
+++ internal/aggregate/controller.go
@@ -10,7 +10,6 @@ import (
 
 	"github.com/dogmatiq/dogma"
 	"github.com/dogmatiq/enginekit/protobuf/envelopepb"
-	"github.com/dogmatiq/enginekit/protobuf/uuidpb"
 	"github.com/dogmatiq/reference-engine/internal/commandqueue"
 	"github.com/dogmatiq/reference-engine/internal/x/xslog"
 	"github.com/dogmatiq/reference-engine/internal/x/xsql"
@@ -27,29 +26,89 @@ type Controller struct {
 
 // Run handles messages for the controller's handler until ctx is canceled.
 func (c *Controller) Run(ctx context.Context) (err error) {
-	for {
-		row := c.DB.QueryRowContext(
+	c.Logger.DebugContext(
+		ctx,
+		"aggregate controller started",
+	)
+
+	defer func() {
+		c.Logger.DebugContext(
 			ctx,
-			`SELECT
-				message_type_id,
-				envelope
-			FROM dogma.pending_commands
-			WHERE message_type_id = ANY($1)
-			LIMIT 1`,
-			c.CommandTypeIDs,
+			"aggregate controller stopped",
+			xslog.Error(err),
 		)
+	}()
 
-		commandEnvelope := &envelopepb.Envelope{}
-		messageTypeID := &uuidpb.UUID{}
+	for {
+		pollImmediately := false
 
-		if err := row.Scan(
-			xsql.UUID(messageTypeID),
-			xsql.Envelope(commandEnvelope),
+		if err := xsql.Transact(
+			ctx,
+			c.DB,
+			func(ctx context.Context, tx *sql.Tx) error {
+				row := tx.QueryRowContext(
+					ctx,
+					`SELECT envelope
+					FROM dogma.pending_commands
+					WHERE message_type_id = ANY($1)
+					LIMIT 1
+					FOR UPDATE SKIP LOCKED`,
+					c.CommandTypeIDs,
+				)
+
+				commandEnvelope := &envelopepb.Envelope{}
+
+				if err := row.Scan(
+					xsql.Envelope(commandEnvelope),
+				); err != nil {
+					if errors.Is(err, sql.ErrNoRows) {
+						return nil
+					}
+
+					return fmt.Errorf("unable to scan pending command: %w", err)
+				}
+
+				commandForRouting, ok, err := c.unpackCommand(ctx, tx, commandEnvelope)
+				if !ok || err != nil {
+					return err
+				}
+
+				instanceID := c.Handler.RouteCommandToInstance(commandForRouting)
+				if instanceID == "" {
+					return errors.New("handler returned an empty instance ID")
+				}
+
+				// Unpack the command a second time, as the Dogma API requires that each
+				// time a message is passed to the application, the application assumes
+				// ownership of the message and the values within it.
+				commandForHandling, ok, err := c.unpackCommand(ctx, tx, commandEnvelope)
+				if !ok || err != nil {
+					return err
+				}
+
+				root := c.Handler.New()
+
+				c.Handler.HandleCommand(
+					root,
+					&scope{
+						instanceID: instanceID,
+					},
+					commandForHandling,
+				)
+
+				pollImmediately = true
+
+				return commandqueue.Remove(
+					ctx,
+					tx,
+					commandEnvelope.GetBody().GetMessageId(),
+				)
+			},
 		); err != nil {
-			if !errors.Is(err, sql.ErrNoRows) {
-				return fmt.Errorf("unable to scan pending command: %w", err)
-			}
+			return err
+		}
 
+		if !pollImmediately {
 			select {
 			case <-ctx.Done():
 				return ctx.Err()
@@ -57,59 +116,28 @@ func (c *Controller) Run(ctx context.Context) (err error) {
 				continue
 			}
 		}
+	}
+}
 
-		commandForRouting, err := envelopepb.Unpack[dogma.Command](commandEnvelope)
-		if err != nil {
-			c.Logger.ErrorContext(
-				ctx,
-				"command cannot be unpacked from envelope",
-				xslog.Envelope(commandEnvelope),
-			)
-
-			if err := commandqueue.Defer(
-				ctx,
-				c.DB,
-				commandEnvelope.GetBody().GetMessageId(),
-			); err != nil {
-				return err
-			}
-
-			continue
-		}
-
-		instanceID := c.Handler.RouteCommandToInstance(commandForRouting)
-		if instanceID == "" {
-			return errors.New("handler returned an empty instance ID")
-		}
-
-		// Unpack the command a second time, as the Dogma API requires that each
-		// time a message is passed to the application, the application assumes
-		// ownership of the message and the values within it.
-		commandForHandling, err := envelopepb.Unpack[dogma.Command](commandEnvelope)
-		if err != nil {
-			return err
-		}
-
-		root := c.Handler.New()
-
-		c.Handler.HandleCommand(
-			root,
-			&scope{
-				instanceID: instanceID,
-			},
-			commandForHandling,
+func (c *Controller) unpackCommand(
+	ctx context.Context,
+	tx *sql.Tx,
+	commandEnvelope *envelopepb.Envelope,
+) (dogma.Command, bool, error) {
+	command, err := envelopepb.Unpack[dogma.Command](commandEnvelope)
+	if err != nil {
+		c.Logger.ErrorContext(
+			ctx,
+			"command cannot be unpacked from envelope",
+			xslog.Envelope(commandEnvelope),
 		)
 
-		return xsql.Transact(
+		return nil, false, commandqueue.Defer(
 			ctx,
-			c.DB,
-			func(ctx context.Context, tx *sql.Tx) error {
-				return commandqueue.Remove(
-					ctx,
-					tx,
-					commandEnvelope.GetBody().GetMessageId(),
-				)
-			},
+			tx,
+			commandEnvelope.GetBody().GetMessageId(),
 		)
 	}
+
+	return command, true, nil
 }
